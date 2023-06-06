e3a350af86b38192aad2ca85ae2debcca46c0609
PB Github <phuong.buitan95@gmail.com>
2023-06-06 13:28:43 +0700

    install react-router library

--- creating-components/src/App.js
+++ creating-components/src/App.js
@@ -6,17 +6,28 @@
 // import ModeToggler from './components/ModeToggler';
 // import Child from './components/Child';
 
-import React, {useState} from 'react';
+// import React, {useState} from 'react';
 // import Lemon from './components/Lemon';
-import MealsProvider from './providers/MealsProvider';
-import Counter from './components/Counter';
-import MealsList from './components/MealsList';
+// import MealsProvider from './providers/MealsProvider';
+// import Counter from './components/Counter';
+// import MealsList from './components/MealsList';
 
+// import {useReducer} from 'react';
+
+import HomePage from './components/HomePage';
+import AboutMe from './components/AboutMe';
+import {Routes, Route} from 'react-router-dom';
 import './App.css';
 
 // function Logo(props) {
 //   const userPic = <img src={avatar} />;
 //   return userPic;
+// }
+
+// const reducer = (state, action) => {
+//   if (action.type === 'ride') return {money: state.money + 10};
+//   if (action.type === 'fuel') return {money: state.money - 50};
+//   return new Error();
 // }
 
 function App() {
@@ -49,15 +60,45 @@ function App() {
   //   </div>
   // )
 
-  return (
-    <div>
-      <MealsProvider>
-        <MealsList />
-        <Counter />
-      </MealsProvider>
-    </div>
-  )
+  // return (
+  //   <div>
+  //     <MealsProvider>
+  //       <MealsList />
+  //       <Counter />
+  //     </MealsProvider>
+  //   </div>
+  // )
 
+  // const initialState = {money: 100};
+  // const [state, dispatch] = useReducer(reducer, initialState);
+
+  // return (
+  //   <div className='App'>
+  //     <h1>Wallet: {state.money}</h1>
+  //     <div>
+  //       <button onClick={() => dispatch({type: 'ride'})}>
+  //         A new customer!
+  //       </button>
+  //       <button onClick={() => dispatch({type: 'fuel'})}>
+  //         Refill the tank!
+  //       </button>
+  //     </div>
+  //   </div>
+  // )
+
+  return (
+    <div className="App">
+      <nav className="nav">
+        <a href="#" className="nav-item">HomePage</a>
+        <a href="#" className="nav-item">About Me</a>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about-me" element={<AboutMe />} />
+      </Routes>
+      
+    </div>
+  );
 }
 
 export default App;
